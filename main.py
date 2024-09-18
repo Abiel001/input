da3cafcd390636acf96d959cefbb79f71ca9f017
Abiel Mainty <maintabi001@warren.k12.in.us>
2024-09-18 07:27:53 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+droids = input("number of droids: ")
+wookiees = input("number of wookiees: ")
+print(name, "wants to meet", droids, "droids and ", wookiees, "Wookiees!")
+
